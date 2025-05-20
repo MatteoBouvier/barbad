d58978068a4d4ad794f20f2a4389cc1d1965a3a6
mbvr <m.bouvier@vidium-solutions.com>
2025-05-21 00:58:48 +0200

    setup CI
    * Create python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * fix(ruff): formatted files
    
    * fix(CI): fix to run pytest in CI
    
    * fix(CI): fix CI runs twice in PR

--- barbad/cli.py
+++ barbad/cli.py
@@ -5,18 +5,11 @@ from barbad.main import OptimizationFlag, main
 
 
 def cli() -> None:
-    parser = argparse.ArgumentParser(
-        "barbad", description="barbad: bytecode optimization for CPython"
-    )
+    parser = argparse.ArgumentParser("barbad", description="barbad: bytecode optimization for CPython")
     parser.add_argument("file", help="program read from script file")
     parser.add_argument("-r", "--run", action="store_true", help="Run optimized code")
     parser.add_argument(
-        "-o",
-        action="extend",
-        choices=["inline"],
-        help="Select optimizations to apply",
-        dest="optimizations",
-        nargs=1,
+        "-o", action="extend", choices=["inline"], help="Select optimizations to apply", dest="optimizations", nargs=1
     )
 
     args = parser.parse_args()
