e836d0bf444fd4f544e54a99b0a6d31f3bcda2a4
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2025-05-24 00:10:34 +0200

    feat(cli): added 'show' argument

--- barbad/cli.py
+++ barbad/cli.py
@@ -1,22 +1,40 @@
 import sys
 import argparse
+from typing import NoReturn, override
 
 from barbad.main import OptimizationFlag, main
 
 
+class CLIParser(argparse.ArgumentParser):
+    @override
+    def error(self, message: str) -> NoReturn:
+        self.print_help(sys.stderr)
+        self.exit(2, f"\nerror: {message}\n")
+
+
 def cli() -> None:
-    parser = argparse.ArgumentParser("barbad", description="barbad: bytecode optimization for CPython")
-    parser.add_argument("file", help="program read from script file")
-    parser.add_argument("-r", "--run", action="store_true", help="Run optimized code")
+    parser = CLIParser("barbad", description="barbad: bytecode optimization for CPython")
+
+    parser.add_argument("file", help="read python code from file or from stdin if '-'")
+    parser.add_argument("-r", "--run", action="store_true", help="run optimized code")
     parser.add_argument(
-        "-o", action="extend", choices=["inline"], help="Select optimizations to apply", dest="optimizations", nargs=1
+        "-s",
+        "--show",
+        choices=["raw", "ast", "bytecode"],
+        help="show raw python code, parsed AST or bytecodes after optimization",
+    )
+    parser.add_argument(
+        "-oinline",
+        action="store_const",
+        const=OptimizationFlag.inline,
+        default=OptimizationFlag.none,
+        help="inline functions marked with the Inline__ return type",
     )
 
     args = parser.parse_args()
 
     optimizations = OptimizationFlag(0)
-    if args.optimizations is not None:
-        for opt in args.optimizations:
-            optimizations |= OptimizationFlag[opt]
+    for opt in ("oinline",):
+        optimizations |= getattr(args, opt)
 
-    sys.exit(main(args.file, optimizations, args.run))
+    sys.exit(main(args.file, optimizations, args.show, args.run))
--- barbad/main.py
+++ barbad/main.py
@@ -1,24 +1,58 @@
 import ast
-from pathlib import Path
+import dis
+import sys
+from contextlib import closing
 from enum import IntFlag, auto
+from pathlib import Path
+from types import CodeType
+from typing import Literal
 
 from barbad.inline import inline
 
 
 class OptimizationFlag(IntFlag):
+    none = 0
     inline = auto()
 
 
-def main(source: str | Path, optimizations: OptimizationFlag, run: bool = False) -> int:
-    source = Path(source)
-    with open(source) as f:
-        tree = ast.parse(f.read(), source.name, "exec")
+def show_code(mode: Literal["raw", "ast", "bytecode"], tree: ast.AST, compiled: CodeType | None):
+    if mode == "raw":
+        print(ast.unparse(tree))
+
+    elif mode == "ast":
+        print(ast.dump(tree, indent=2))
+
+    elif mode == "bytecode":
+        assert compiled is not None
+        dis.dis(compiled)
+
+
+def main(
+    source: str | Path,
+    optimizations: OptimizationFlag,
+    show: Literal["raw", "ast", "bytecode"] | None,
+    run: bool = False,
+) -> int:
+    input_code = sys.stdin if source == "-" else open(source)
+    input_name = "<string>" if source == "-" else Path(source).name
+
+    with closing(input_code):
+        tree = ast.parse(input_code.read(), input_name, "exec")
 
     if optimizations & OptimizationFlag.inline:
         inline(tree).or_exit()
 
+    if run or show == "bytecode":
+        comp = compile(tree, input_name, "exec")
+
+    else:
+        comp = None
+
+    if show is not None:
+        show_code(show, tree, comp)
+
     if run:
-        print(ast.dump(tree, indent=2))
-        exec(compile(tree, source.name, "exec"), globals())
+        assert comp is not None
+        exec(comp, globals())
 
     return 0
